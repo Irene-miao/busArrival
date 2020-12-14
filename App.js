2258367a7945ccda8d0194422a34f0da740d1966
Irene <irene_lee@live.com.sg>
2020-12-14 11:16:32 +0800

    Added button and text

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>Bus arrival times</Text>
+      <Text style={styles.sub}>Loading..</Text>
+     <TouchableOpacity
+      style={styles.button}
+     >
+     <Text style={styles.buttonText}>Refreshed!</Text>
+     </TouchableOpacity>
+     
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +24,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+fontSize: 40,
+margin: 10,
+  },
+  sub: {
+fontSize: 30,
+margin: 10,
+  },
+  button: {
+    textAlign: 'center',
+    backgroundColor: 'black',
+    borderRadius: 10,
+    margin: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    padding: 10,
   },
 });
